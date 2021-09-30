6f6ddb10da6c1f01df7f3f8e8267d2595e3b11fc
buyuktas18 <dbuyuktas.gsl@gmail.com>
2021-09-30 08:51:09 +0300

    ui is updated

--- executable/tkinter_tutorial.py
+++ executable/tkinter_tutorial.py
@@ -4,12 +4,17 @@ from tkinter.messagebox import showinfo
 from data_tool import run_program
 import threading
 import time
+import re
+from show_position import create_plot
 
+
+flag = 0
 window = tk.Tk()
 window.title('White Balance Data Analyse Tool')
 window.geometry('600x400')
 files = ''
 filename = []
+path = ''
 
 variables = [0, 0]
 var1 = tk.IntVar()
@@ -53,29 +58,66 @@ def select_files():
 def update_label():
     global files
     label['text'] = files
-
-def count_time(thr):
-    while(thr.is_alive()):
+"""
+def count_time(thr1, thr2):
+    global flag
+    thr.start()
+    while(thr1.is_alive()):
         t = int(time_label['text'])
         time.sleep(1)
         t = t+1
         time_label['text'] = t
+    
+    flag = True
+    """
 
 def start_program():
     global filename
     global variables
+    global path
+    global flag
     path = fd.askdirectory()
     print(path)
     #multithreading for not responding problem
-    thread1 = threading.Thread(target=run_program, args=(filename, path, variables)) 
-    thread2 = threading.Thread(target=count_time, args=[thread1])
-    thread1.start()
-    thread2.start()
+    """thread1 = threading.Thread(target=run_program, args=(filename, path, variables,))
+    thread2 = threading.Thread(target=openNewWindow, args=(filename, thread1,))"""
+    
+    run_program(filename, path, variables)
+    openNewWindow(filename)
+
+    """while(thread1.is_alive()):
+        print("Waiting")
+        time.sleep(1)
+        t = int(time_label['text'])
+        time.sleep(1)
+        t = t+1
+        time_label['text'] = t
+    openNewWindow(filename)
+    thread1.join()"""
+    
+    
+
+def openNewWindow(options_arr):
+
+
+
+
+    global path
+    
+    tk.Button(text=options_arr[0], command = lambda: save_plot(path+"/057D40-SW4-NG")).pack()
+    tk.Button(text=options_arr[1], command = lambda: save_plot(path+"/057D43-SW5-NG")).pack()
+    tk.Button(text=options_arr[2], command = lambda: save_plot(path+"/057E32-SW4-NF")).pack()
+     
+def save_plot(strf):
+    create_plot(strf)
+
 
 time_label = tk.Label(
     window,
     text="0"
 )
+
+
 
 run_button = tk.Button(
     window,
